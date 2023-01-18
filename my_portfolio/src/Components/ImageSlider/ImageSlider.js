93aa6d9af4d08a777fa861c1f2b198400ca3b60c
Tomas Krinickas <tomas.krinickas2@gmail.com>
2023-01-18 22:12:47 +0200

    commit

--- my_portfolio/src/Components/ImageSlider/ImageSlider.js
+++ my_portfolio/src/Components/ImageSlider/ImageSlider.js
@@ -1,6 +1,47 @@
-import '../ImageSlider/ImageSlider.css';
 import { useState } from "react"
 import { FaArrowAltCircleRight, FaArrowAltCircleLeft } from "react-icons/fa";
+import styled from 'styled-components';
+
+const SliderDiv = styled.div`
+  position: relative;
+  height: 40vh;
+  width: 750px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const LeftArrow = styled.span`
+  display: flex;
+  position: absolute;
+  top: 50%;
+  left: 0px;
+  font-size: 3rem;
+  color: black;
+  z-index: 10;
+  cursor: pointer;
+`;
+
+const RightArrow = styled.span`
+  display: flex;
+  position: absolute;
+  top: 50%;
+  right: 0px;
+  font-size: 3rem;
+  color: black;
+  z-index: 10;
+  cursor: pointer;
+  border-radius: 55%;
+  border: none;
+  width: fit-content;
+`;
+
+const Image = styled.img`
+    width: 700px;
+    height: 400px;
+    border-radius: 10px;
+`;
+
 const ImageSlider = ({picturesArr}) => {
     const [current, setCurrent] = useState(0);
     const length = picturesArr.length;
@@ -18,18 +59,21 @@ const ImageSlider = ({picturesArr}) => {
     }
 
   return (
-    <div className="slider">
-        <FaArrowAltCircleLeft className="left-arrow" onClick={prevSlide}/>
+    <SliderDiv>
+        <LeftArrow>
+        <FaArrowAltCircleLeft onClick={prevSlide}/>
+        </LeftArrow>
+        <RightArrow>
         <FaArrowAltCircleRight className="right-arrow" onClick={nextSlide}/>
-      {picturesArr.map((pic, index)=>{
-        
+        </RightArrow>
+        {picturesArr.map((pic, index)=>{
         return(
           <div className={index === current ? 'slide active' : 'slide'}>
-            {index === current && (<img key={index} src={pic.image} alt="projectImage" className='image'/>)}
+            {index === current && (<Image key={index} src={pic.image} alt="projectImage"/>)}
           </div>
         )
-      })}
-    </div>
+        })}
+    </SliderDiv>
   )
 }
 
